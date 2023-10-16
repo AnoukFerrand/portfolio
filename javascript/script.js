0216622200ec4fb74e3bf908b1adca7f480f7ea2
AnoukFerrand <anoukfrd@gmail.com>
2023-10-16 10:51:35 +0200

    organised and cleaned code + size update animation profil picture circle

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,10 @@
 document.addEventListener("DOMContentLoaded", (event) => {
 
-    // menu burger animation
     let menu__links = document.querySelector('#menu__links__burger');
     let openBtn = document.querySelector('#openBtn');
     let closeBtn = document.querySelector('#closeBtn');
 
-
-
-
+    // menu burger animation
     openBtn.addEventListener('click', () => {
         menu__links.classList.add('active');
     });
@@ -16,17 +13,19 @@ document.addEventListener("DOMContentLoaded", (event) => {
         menu__links.classList.remove('active');
     });
 
+    // Call Animation profil picture
     profilPictureMouseEffect();
 
 });
 
+// Animation profil picture
 function profilPictureMouseEffect() { 
     let imgProfil = document.querySelector('.profil');
     let isIn = false;
 
     imgProfil.addEventListener('mouseenter', () => {
         isIn = true;
-        imgProfil.style.setProperty('--profil-img-size', '130px');
+        imgProfil.style.setProperty('--profil-img-size', '150px'); // change px to modifie mouse circle size
 
     });
 
@@ -45,33 +44,5 @@ function profilPictureMouseEffect() {
             imgProfil.style.setProperty('--profil-img-y', y + 'px');
 
         }
-    });
-
-}
-
-
-
-
-
-// fonction test
-function testImageEffect() {
-    let blur = document.querySelector('.blur');
-    let imgProfil = document.querySelector('.profil');
-    imgProfil.addEventListener('mousemove', (e) => {
-
-        let rect = imgProfil.getBoundingClientRect();
-        let x = e.clientX - rect.left - 50;
-        let y = e.clientY - rect.top - 50;
-
-        blur.style.top = y + 'px';
-        blur.style.left = x + 'px'; 
-    });
-
-    imgProfil.addEventListener('mouseenter', () => {
-        blur.style.display = 'block';
-    });
-
-    imgProfil.addEventListener('mouseleave', () => {
-        blur.style.display = 'none';
     });
 }
